906e8786aa2d9135bd8444798d6b93a695d4bd1d
emmanuel ronoh <eronoh036@gmail.com>
2024-09-20 17:49:26 +0300

    first commit

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,34 @@
 class MySet:
+    def __init__(self, initial_values=None):
+        """Initialize the set with unique values from a list."""
+        self.dictionary = {}
+        if initial_values is not None:
+            for value in initial_values:
+                self.add(value)
 
-    pass
+    def add(self, value):
+        """Add a value to the set if it is not already present."""
+        if value not in self.dictionary:
+            self.dictionary[value] = True
+
+    def delete(self, value):
+        """Remove a value from the set if it exists."""
+        if value in self.dictionary:
+            del self.dictionary[value]
+
+    def has(self, value):
+        """Check if the set contains a value."""
+        return value in self.dictionary
+
+    def size(self):
+        """Return the number of elements in the set."""
+        return len(self.dictionary)
+
+    def clear(self):
+        """Clear all elements from the set."""
+        self.dictionary.clear()
+
+    def __str__(self):
+        """Return a string representation of the set."""
+        elements = ','.join(map(str, self.dictionary.keys()))
+        return f'MySet: {{{elements}}}'
